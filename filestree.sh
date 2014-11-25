411af71b92f66724e4749d5ad091a5a715367b4a
gerard <gtionogu@ulb.ac.be>
2014-11-25 08:31:00 +0100

    Added comment

--- filestree.sh
+++ filestree.sh
@@ -7,11 +7,11 @@ parcours(){
 	for element in $1/*
 	do
 		# checking if the element exists and isn't a symbolical link
-		if [ -e $element -a $(ls -l $element | grep ^l | wc -l) == 0 ]; then
+		if [ -e $element -a $(ls -l $element | grep ^l | wc -l) == 0 ]; then 
 			#directory
 			if [ -d $element ]; then
 				#not empty
-				if [ $(ls -l $element | grep -v ^total | wc -l) -ge 1 ]; then
+				if [ $(ls -l $element | grep -v ^total | wc -l) -ge 1 ]; then 
 					echo "$2|-- ${element#$1/}"
 					parcours "$element" "$2    "
 				#empty
