b13d67b36d72ab58cb1d6dc25bab36e2694495f5
gerard <gtionogu@ulb.ac.be>
2014-11-27 10:50:01 +0100

    Added file with Spaces and fixed a if-elseif bug

--- filestree.sh
+++ filestree.sh
@@ -1,36 +1,44 @@
+# Gérard Tio Nogueras - INFO2 - 000333083
+# Info-f-201
+# Projet n°1: Shell Scripting
+
 #! bin/bash
 
 parcours(){
 	if [[ "$1" == "." ]]; then
 			echo $PWD
 	fi
-	for element in $1/*
+	for element in "$1"/*
+
 	do
-		# checking if the element exists and isn't a symbolical link
-		if [ -e $element -a $(ls -l $element | grep ^l | wc -l) == 0 ]; then 
-			#directory
-			if [ -d $element ]; then
-				#not empty
-				if [ $(ls -l $element | grep -v ^total | wc -l) -ge 1 ]; then 
+		#echo "element: $element"
+		# checking if it isn't a symbolical link
+		if [ ! -L "$element" ]; then #$(ls -l "$element" | grep ^l | wc -l) == 0
+			# Is it a directory ?
+			if [ -d "$element" ]; then
+				#echo "dossier"
+				# Is it not empty ?
+				if [ $(ls -l "$element" | grep -v ^total | wc -l) -ge 1 ]; then #-x "$element" -a
 					echo "$2|-- ${element#$1/}"
 					parcours "$element" "$2    "
-				#empty
+				# Then it's empty
 				else
 					echo "$2|-- ${element#$1/}"
 					echo "$2    |-- *"	
 				fi
-			fi
-			#file
-			if [ -f $element ]; then
+			# Is it a file ?
+			elif [ -f "$element" ]; then
+				#echo "fichier"
 				echo "$2|-- ${element#$1/}"
 			fi
+		#echo "Where : $1"
 		fi
 	done
 }
 
 root="."
-#testing an argument has been given
-[ $# -ge 1 ] && root="$1"
+#testing an argument has been given and that it exists
+[ $# -ge 1 ] && [ -e "$1" ] && root="$1"
 parcours "$root"
 
 
